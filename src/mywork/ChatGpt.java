662e06f7c588c36121a8571493db007ae0877fa8
FigurePainter <shalyanharutyun9@gmail.com>
2025-10-25 14:44:00 +0400

    Initial commit

---
+++ src/mywork/ChatGpt.java
@@ -0,0 +1,14 @@
+package mywork;
+
+public class ChatGpt {
+    public static void main(String[] args) {
+        int n = -5;
+        if (n == 0) {
+            System.out.println("sifr");
+        } else if (n > 0) {
+            System.out.println("drakan");
+        } else {
+            System.out.println("bacasakan");
+        }
+    }
+}
